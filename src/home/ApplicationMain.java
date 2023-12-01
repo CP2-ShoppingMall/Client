7f09bbe4df5deca5e25d7211974ee327de544f35
LAZYCAT\user <mylazysundaycat@gmail.com>
2023-12-01 15:05:11 +0900

    Create. Register page UI 제작 완료

--- src/home/ApplicationMain.java
+++ src/home/ApplicationMain.java
@@ -14,37 +14,37 @@ public class ApplicationMain extends JFrame{
         setTitle("CP2-ShoppingMall");
         setSize(500,1020);
 
-        //헤드
-        HeadPanel head_panel = new HeadPanel();
-        head_panel.setBounds(0,0,500,200);
-        add(head_panel);
-
-        //바디
-        ///TODO 해당 코드에서 페이지가 바뀝니다. 상품 페이지 / 회원가입페이지 / 상품 상세페이지 ...
-        //1. 상품 리스트 페이지
-//        ListPanel list_panel = new ListPanel();
-//        list_panel.setBounds(0,200,500,700);
-//        add(list_panel);
-        //2. 상품 상세 페이지
-//        ProductPanel product_panel = new ProductPanel();
-//        product_panel.setBounds(0,200,500,700);
-//        add(product_panel);
-//       //3. 판매자 글 작성 페이지
-//        WritePostPanel write_post_panel = new WritePostPanel();
-//        write_post_panel.setBounds(0,200,500,700);
-//        add(write_post_panel);
-        //4. 장바구니 페이지
-        BasketPanel basket_panel
-                = new BasketPanel();
-        basket_panel.setBounds(0,200,500,700);
-        add(basket_panel);
-
-
-        //푸터
-        FootPanel foot_panel = new FootPanel();
-        foot_panel.setBounds(0,880,500,100);
-        add(foot_panel);
-        setVisible(true);
+//        //헤드
+//        HeadPanel head_panel = new HeadPanel();
+//        head_panel.setBounds(0,0,500,200);
+//        add(head_panel);
+//
+//        //바디
+//        ///TODO 해당 코드에서 페이지가 바뀝니다. 상품 페이지 / 회원가입페이지 / 상품 상세페이지 ...
+//        //1. 상품 리스트 페이지
+////        ListPanel list_panel = new ListPanel();
+////        list_panel.setBounds(0,200,500,700);
+//////        add(list_panel);
+////        //2. 상품 상세 페이지
+////        ProductPanel product_panel = new ProductPanel();
+////        product_panel.setBounds(0,200,500,700);
+//////        add(product_panel);
+//////       //3. 판매자 글 작성 페이지
+////        WritePostPanel write_post_panel = new WritePostPanel();
+////        write_post_panel.setBounds(0,200,500,700);
+//////        add(write_post_panel);
+////        //4. 장바구니 페이지
+////        BasketPanel basket_panel
+////                = new BasketPanel();
+////        basket_panel.setBounds(0,200,500,700);
+////        add(basket_panel);
+//
+//
+//        //푸터
+//        FootPanel foot_panel = new FootPanel();
+//        foot_panel.setBounds(0,880,500,100);
+//        add(foot_panel);
+//        setVisible(true);
 
 //        //4. 메인 페이지
 //        //TODO 메인페이지 실행시에 위에 헤드, 바디, 푸터 모두 주석처리하고 실행
@@ -52,6 +52,11 @@ public class ApplicationMain extends JFrame{
 //        mainpage_panel.setBounds(0,0,500,1200);
 //        add(mainpage_panel);
 //        setVisible(true);
+        //5. 회원가입 페이지
+        RegisterPanel register_panel = new RegisterPanel();
+        register_panel.setBounds(0,0,500,1200);
+        add(register_panel);
+        setVisible(true);
 
 
 
--- src/home/RegisterPanel.java
+++ src/home/RegisterPanel.java
@@ -12,10 +12,10 @@ import java.awt.*;
 public class RegisterPanel extends JPanel {
     ImageIcon logo_icon;
     Image logo_img;
-    JLabel logo, id, pwd;
-    RoundJTextField id_txt;
+    JLabel logo, id, pwd, phone, address, email;
+    RoundJTextField id_txt, phone_txt, address_txt, email_txt;
     RoundJPassWordField pwd_txt;
-    RoundedButton login_btn,sign_btn;
+    RoundedButton sign_btn;
 
 
     public RegisterPanel(){
@@ -33,13 +33,13 @@ public class RegisterPanel extends JPanel {
         logo_img = logo_img.getScaledInstance(410,160,Image.SCALE_SMOOTH);
         logo_icon = new ImageIcon(logo_img);
         logo = new JLabel(logo_icon);
-        logo.setBounds(40,250, 410, 160);
+        logo.setBounds(40,70, 410, 160);
         add(logo);
 
 
         //아이디 라벨
         id = new JLabel("아이디");
-        id.setBounds(80,410,80,80);
+        id.setBounds(50,250,80,80);
         id.setFont(new Font("마루 부리OTF",Font.BOLD,22));
         id.setForeground(new Color(33,39,37));
         add(id);
@@ -47,7 +47,7 @@ public class RegisterPanel extends JPanel {
         //아이디
         id_txt = new RoundJTextField(30);
         id_txt.setBackground(new Color(157,184,177));
-        id_txt.setBounds(180,425,230,50);
+        id_txt.setBounds(170,265,250,50);
         id_txt.setFont(new Font("마루 부리OTF",Font.PLAIN,22));
         id_txt.setText("");
         id_txt.setBorder(new LineBorder(new Color(157,184,177)));
@@ -55,7 +55,7 @@ public class RegisterPanel extends JPanel {
 
         //비밀번호 라벨
         pwd = new JLabel("비밀번호");
-        pwd.setBounds(60,490,120,80);
+        pwd.setBounds(50,320,120,80);
         pwd.setFont(new Font("마루 부리OTF",Font.BOLD,22));
         pwd.setForeground(new Color(33,39,37));
         add(pwd);
@@ -63,20 +63,60 @@ public class RegisterPanel extends JPanel {
         //비밀번호
         pwd_txt = new RoundJPassWordField(30);
         pwd_txt.setBackground(new Color(157,184,177));
-        pwd_txt.setBounds(180,505,230,50);
+        pwd_txt.setBounds(170,335,250,50);
         pwd_txt.setFont(new Font("마루 부리OTF",Font.PLAIN,22));
         pwd_txt.setText("");
         pwd_txt.setBorder(new LineBorder(new Color(157,184,177)));
         add(pwd_txt);
 
-        //TODO 서버연결
-        //로그인
-        login_btn = new RoundedButton("로그인");
-        login_btn.setBackground(new Color(33,39,37));
-        login_btn.setForeground(Color.white);
-        login_btn.setFont(new Font("맑은 고딕", Font.BOLD, 22));
-        login_btn.setBounds(90,605,135,55);
-        add(login_btn);
+        //이메일 라벨
+        email = new JLabel("이메일");
+        email.setBounds(50,390,120,80);
+        email.setFont(new Font("마루 부리OTF",Font.BOLD,22));
+        email.setForeground(new Color(33,39,37));
+        add(email);
+
+        //이메일
+        email_txt = new RoundJTextField(30);
+        email_txt.setBackground(new Color(157,184,177));
+        email_txt.setBounds(170,405,250,50);
+        email_txt.setFont(new Font("마루 부리OTF",Font.PLAIN,22));
+        email_txt.setText("");
+        email_txt.setBorder(new LineBorder(new Color(157,184,177)));
+        add(email_txt);
+
+        //폰번호 라벨
+        phone = new JLabel("폰 번호");
+        phone.setBounds(50,460,120,80);
+        phone.setFont(new Font("마루 부리OTF",Font.BOLD,22));
+        phone.setForeground(new Color(33,39,37));
+        add(phone);
+
+        //폰번호
+        phone_txt = new RoundJTextField(30);
+        phone_txt.setBackground(new Color(157,184,177));
+        phone_txt.setBounds(170,475,250,50);
+        phone_txt.setFont(new Font("마루 부리OTF",Font.PLAIN,22));
+        phone_txt.setText("");
+        phone_txt.setBorder(new LineBorder(new Color(157,184,177)));
+        add(phone_txt);
+
+        //주소 라벨
+        address = new JLabel("집 주소");
+        address.setBounds(50,535,120,80);
+        address.setFont(new Font("마루 부리OTF",Font.BOLD,22));
+        address.setForeground(new Color(33,39,37));
+        add(address);
+
+        //주소
+        address_txt = new RoundJTextField(30);
+        address_txt.setBackground(new Color(157,184,177));
+        address_txt.setBounds(170,550,250,50);
+        address_txt.setFont(new Font("마루 부리OTF",Font.PLAIN,22));
+        address_txt.setText("");
+        address_txt.setBorder(new LineBorder(new Color(157,184,177)));
+        add(address_txt);
+
 
         //TODO 서버연결
         //회원가입
@@ -84,8 +124,9 @@ public class RegisterPanel extends JPanel {
         sign_btn.setBackground(new Color(33,39,37));
         sign_btn.setForeground(Color.white);
         sign_btn.setFont(new Font("맑은 고딕", Font.BOLD, 22));
-        sign_btn.setBounds(260,605,135,55);
+        sign_btn.setBounds(140,650,210,55);
         add(sign_btn);
+
 
         setVisible(true);
     }
